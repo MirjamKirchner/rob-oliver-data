1786caaef4844fa26d23e43cf98a0c58a7f42ba9
Mirjam Kirchner <riedel.ks@gmail.com>
2022-07-26 23:30:06 +0200

    #Incomplete: Added Clear-ML version control. Documentation missing.

--- src/RobEngineer.py
+++ src/RobEngineer.py
@@ -16,20 +16,35 @@ PATH_TO_ROB = os.path.join(PATH_TO_DATA, "interim/rob.csv")
 PATH_TO_ROB_ENGINEERED = os.path.join(PATH_TO_DATA, "processed/rob_engineered.csv")
 PATH_TO_FINDING_PLACES = os.path.join(PATH_TO_DATA, "processed/catalogued_finding_places.csv")
 
-DF_FINDING_PLACES = pd.read_csv(PATH_TO_FINDING_PLACES, dtype={"Long": "float64", "Lat": "float64"})
-MAPPING_FINDING_PLACES = dict(zip(DF_FINDING_PLACES["Name"], DF_FINDING_PLACES.index))
-
 
 class RobGui(PandasGui):
+    """TODO comment"""
+    def __init__(self, path_to_rob_engineered: str, path_to_finding_places: str, settings={}, **kwargs):
+        """
+        TODO comment
+        :param path_to_rob_engineered:
+        :param path_to_finding_places:
+        :param settings:
+        :param kwargs:
+        """
+        self.path_to_rob_engineered = path_to_rob_engineered
+        self.path_to_finding_places = path_to_finding_places
+        super().__init__(settings=settings, **kwargs)
+
     def closeEvent(self, e: QtGui.QCloseEvent) -> None:
+        # Update and save rob_engineered
         df_engineered_rob = self.get_dataframes()["rob_engineered"]
         df_engineered_rob.drop("Fundort", axis=1, inplace=True)
         df_engineered_rob.rename(columns={"Mapped Fundort": "Fundort"}, inplace=True)
-        # TODO check if a new location has been added to the dataframe and if so update catalogued finding places
-
         df_engineered_rob.sort_values(
             by=["Sys_aktualisiert_am", "Einlieferungsdatum"]
-        ).to_csv(PATH_TO_ROB_ENGINEERED, index=False)
+        ).to_csv(self.path_to_rob_engineered, index=False)
+
+        # Update and save catalogued_finding_places
+        df_finding_places = df_engineered_rob[["Fundort", "Lat", "Long"]].dropna().drop_duplicates()
+        df_finding_places.rename(columns={"Fundort": "Name"}, inplace=True)
+        df_finding_places.reset_index(drop=True, inplace=True)
+        df_finding_places.sort_values(by="Name").to_csv(self.path_to_finding_places, index=False)
 
         super().closeEvent(e)
 
@@ -38,21 +53,28 @@ class RobEngineer:
     """
     TODO Comment
     """
-    def __init__(self, earliest_update: datetime = pd.to_datetime("1990-04-30")):
+    def __init__(self, latest_update: datetime = pd.to_datetime("1990-04-30"),
+                 path_to_rob: str = PATH_TO_ROB,
+                 path_to_rob_engineered: str = PATH_TO_ROB_ENGINEERED,
+                 path_to_finding_places: str = PATH_TO_FINDING_PLACES):
         """
         TODO Comment
-        :param earliest_update:
+        :param latest_update:
         """
-        self.earliest_update = earliest_update
-        df_historized_rob = (pd.read_csv(PATH_TO_ROB,
+        self.latest_update = latest_update
+        self.path_to_rob = path_to_rob
+        self.path_to_rob_engineered = path_to_rob_engineered
+        self.path_to_finding_places = path_to_finding_places
+
+        df_historized_rob = (pd.read_csv(self.path_to_rob,
                                          dtype={"Sys_geloescht": "int32"},
                                          parse_dates=["Einlieferungsdatum",
                                                       "Erstellt_am",
                                                       "Sys_aktualisiert_am"],
                                          date_parser=pd.to_datetime)
-                             [lambda x: x["Sys_aktualisiert_am"] >= earliest_update])
+                             [lambda x: x["Sys_aktualisiert_am"] > latest_update])
 
-        self.df_engineered_rob = (pd.read_csv(PATH_TO_ROB_ENGINEERED,
+        self.df_engineered_rob = (pd.read_csv(self.path_to_rob_engineered,
                                               dtype={
                                                   "Long": "float64",
                                                   "Lat": "float64",
@@ -62,7 +84,7 @@ class RobEngineer:
                                                            "Erstellt_am",
                                                            "Sys_aktualisiert_am"],
                                               date_parser=pd.to_datetime)
-                                  [lambda x: x["Sys_aktualisiert_am"] < earliest_update])
+                                  [lambda x: x["Sys_aktualisiert_am"] <= latest_update])
         self.df_engineered_rob = pd.concat([self.df_engineered_rob, df_historized_rob], axis=0)
 
     def _geoparse_rob(self) -> None:
@@ -74,17 +96,20 @@ class RobEngineer:
                                       column="Mapped Fundort",
                                       value=np.nan)
 
-        slice = self.df_engineered_rob["Sys_aktualisiert_am"] >= self.earliest_update
+        slice = self.df_engineered_rob["Sys_aktualisiert_am"] >= self.latest_update
         indices = self.df_engineered_rob.loc[slice, "Fundort"].dropna().index
+
+        df_finding_places = pd.read_csv(self.path_to_finding_places, dtype={"Long": "float64", "Lat": "float64"})
+        mapping_finding_places = dict(zip(df_finding_places["Name"], df_finding_places.index))
 
         for idx in indices:
             closest_match = \
-                difflib.get_close_matches(self.df_engineered_rob.at[idx, "Fundort"], DF_FINDING_PLACES["Name"],
+                difflib.get_close_matches(self.df_engineered_rob.at[idx, "Fundort"], df_finding_places["Name"],
                                           n=1,
                                           cutoff=0.0)[0]
             self.df_engineered_rob.at[idx, "Mapped Fundort"] = closest_match
-            self.df_engineered_rob.at[idx, "Lat"] = DF_FINDING_PLACES.at[MAPPING_FINDING_PLACES[closest_match], "Lat"]
-            self.df_engineered_rob.at[idx, "Long"] = DF_FINDING_PLACES.at[MAPPING_FINDING_PLACES[closest_match], "Long"]
+            self.df_engineered_rob.at[idx, "Lat"] = df_finding_places.at[mapping_finding_places[closest_match], "Lat"]
+            self.df_engineered_rob.at[idx, "Long"] = df_finding_places.at[mapping_finding_places[closest_match], "Long"]
 
     def _show_rob_engineered(self, settings={}, **kwargs) -> RobGui:
         """
@@ -107,7 +132,7 @@ class RobEngineer:
 
         kwargs = {**kwargs, **items}
 
-        pandas_gui = RobGui(settings=settings, **kwargs)
+        pandas_gui = RobGui(self.path_to_rob_engineered, self.path_to_finding_places, settings=settings, **kwargs)
         pandas_gui.caller_stack = inspect.currentframe().f_back
 
         # Register IPython magic
@@ -126,9 +151,18 @@ class RobEngineer:
 
         return pandas_gui
 
+    def engineer_rob(self):
+        """
+        TODO Comment
+        :return:
+        """
+        self._geoparse_rob()
+        self._show_rob_engineered()
+
 
 if __name__ == "__main__":
     #from pandasgui.datasets import pokemon
-    rob_engineer = RobEngineer(earliest_update=pd.to_datetime("2022-07-01"))
-    rob_engineer._geoparse_rob()
-    rob_engineer._show_rob_engineered()
+    #rob_gui = RobGui("test1", "test2", items={"pokemon": pokemon})
+
+    rob_engineer = RobEngineer()
+    rob_engineer.engineer_rob()
